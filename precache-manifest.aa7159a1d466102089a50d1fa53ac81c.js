aee01867717e011be80a8440dde4c949d7b880e6
Ale Piumetti <alepiumetti@hotmail.com>
2020-01-17 00:14:52 -0300

    Updates

---
+++ precache-manifest.aa7159a1d466102089a50d1fa53ac81c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1693690aded1a4874707edb1c9242d66",
+    "url": "/calculadora-simple-react/index.html"
+  },
+  {
+    "revision": "2e14558faa502913f9f1",
+    "url": "/calculadora-simple-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "595698351cd9643726db",
+    "url": "/calculadora-simple-react/static/js/2.01c7fb38.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/calculadora-simple-react/static/js/2.01c7fb38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e14558faa502913f9f1",
+    "url": "/calculadora-simple-react/static/js/main.9f3d031a.chunk.js"
+  },
+  {
+    "revision": "6c78aa90c3edf90ca476",
+    "url": "/calculadora-simple-react/static/js/runtime-main.d6e89d66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculadora-simple-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
